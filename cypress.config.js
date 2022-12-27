09d0a7014a924ec96bcb04e7e2a6cd3ca16f26a4
jordan.gonzalez <jordan.olaves91@gmail.com>
2022-12-26 23:35:40 -0500

    fixing issues

--- cypress.config.js
+++ cypress.config.js
@@ -6,6 +6,7 @@ module.exports = defineConfig ({
   e2e: {
     baseUrl: 'https://www.demoblaze.com/',
     specPattern: 'cypress/e2e/**/*.*.{js,jsx,ts,tsx}',
-    excludeSpecPattern: ['**/1-getting-started/*', '**/2-advanced-examples/*']
+    excludeSpecPattern: ['**/1-getting-started/*', '**/2-advanced-examples/*'],
+    supportFolder: false
   },
 });
